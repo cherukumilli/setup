bd4428da794a974eed791926a76414a685941a52
cherukumilli <cherukumilli2@gmail.com>
2014-07-09 17:09:14 -0700

    fixed nvm install bug

--- setup.sh
+++ setup.sh
@@ -6,12 +6,13 @@
 # https://github.com/creationix/nvm
 sudo apt-get install -y git
 sudo apt-get install -y curl
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+curl https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | bash
+#curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+nvm install v0.10
+nvm use v0.10
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
