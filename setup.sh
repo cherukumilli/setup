65580c68733d8baafea9fffea5bcf6c300ff4dbc
cherukumilli <cherukumilli2@gmail.com>
2014-07-21 10:04:41 -0700

    Added Atom text editor install to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Simple setup.sh for configuring Ubuntu 14.04 LTS development environment
-# for headless setup. 
+# for headless setup.
 
 echo "The script will install the following:"
 echo "1. Oracle JDK 6 and 7 because OpenJDK has intermittent performance issues"
@@ -14,52 +14,73 @@ echo "8. rlwrap"
 echo "9. emacs and its addons"
 echo "10. Heroku toolbelt"
 echo "11. Standard config files"
+echo "12. Install the Atom editor for HTML5 app development"
 read -p "Press [Enter] key to continue or [Cancel] to quit"
 
-# Install Oracle JDK 6 and 7 because OpenJDK has intermittent performance issues"
+echo "*****************************************************************"
+echo "Install Oracle JDK 6 and 7 because OpenJDK has intermittent performance issues"
+echo "*****************************************************************"
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get install oracle-java6-installer # install oracle jdk 6
 sudo apt-get install oracle-java7-installer # install oracle jdk 7
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
+echo "*****************************************************************"
+echo "Install nvm: node-version manager"
+echo "https://github.com/creationix/nvm"
+echo "*****************************************************************"
 sudo apt-get install -y git
 sudo apt-get install -y curl
 curl https://raw.githubusercontent.com/creationix/nvm/v0.10.0/install.sh | bash
 #curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+echo "*****************************************************************"
+echo "Load nvm and install latest production node"
+echo "*****************************************************************"
 source $HOME/.nvm/nvm.sh
 nvm install v0.10
 nvm use v0.10
 
-# install npm
+echo "*****************************************************************"
+echo "install npm"
+echo "*****************************************************************"
 sudo apt-get install npm
 
-# install screen
+echo "*****************************************************************"
+echo "install screen"
+echo "*****************************************************************"
 sudo apt-get install screen
 
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
+echo "*****************************************************************"
+echo "Install jshint to allow checking of JS code within emacs"
+echo "http://jshint.com/"
+echo "*****************************************************************"
 npm install -g jshint
 
-# Install rlwrap to provide libreadline features with node
-# See: http://nodejs.org/api/repl.html#repl_repl
+echo "*****************************************************************"
+echo "Install rlwrap to provide libreadline features with node"
+echo "See: http://nodejs.org/api/repl.html#repl_repl"
+echo "*****************************************************************"
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
+echo "*****************************************************************"
+echo "Install emacs24"
+echo "https://launchpad.net/~cassou/+archive/emacs"
+echo "*****************************************************************"
 sudo add-apt-repository -y ppa:cassou/emacs
 sudo apt-get -qq update
 sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
 
-# Install Heroku toolbelt
-# https://toolbelt.heroku.com/debian
+echo "*****************************************************************"
+echo "Install Heroku toolbelt"
+echo "https://toolbelt.heroku.com/debian"
+echo "*****************************************************************"
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-# git pull and install dotfiles as well
+echo "*****************************************************************"
+echo "git pull and install dotfiles as well"
+echo "*****************************************************************"
 cd $HOME
 if [ -d ./dotfiles/ ]; then
     mv dotfiles dotfiles.old
@@ -74,7 +95,20 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+echo "*****************************************************************"
 echo "Please select the latest oracle java version from the list below (openjdk has problems)"
+echo "*****************************************************************"
 read -p "Press [Enter] key to continue"
 sudo update-alternatives --config java
 sudo update-alternatives --config javac
+
+echo "*****************************************************************"
+echo "Install Atom Editor for HTML5 app development. (https://atom.io/)"
+echo "*****************************************************************"
+sudo apt-get install build-essential git libgnome-keyring-dev
+git clone https://github.com/atom/atom
+cd atom
+script/build # Creates application at $TMPDIR/atom-build/Atom
+sudo script/grunt install # Installs command to /usr/local/bin/atom
+script/grunt mkdeb # Generates a .deb package at $TMPDIR/atom-build, e.g. /tmp/atom-build
+atom #launch the atom text editor
